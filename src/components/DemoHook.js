f52e0c41f1cc83666ce0b7c2023255d1acc94715
bhumit-bacancy <bhumit.bhadani@bacancy.com>
2024-01-09 16:40:59 +0530

    hooks backup

--- src/components/DemoHook.js
+++ src/components/DemoHook.js
@@ -1,44 +1,24 @@
 import React, { useEffect, useState } from "react";
 
 export default function DemoHook() {
-  // const [resourceType, setResourceType] = useState("posts");
-  // const [items, setItems] = useState([])
-  // useEffect(() => {
-  //   fetch(`https://jsonplaceholder.org/${resourceType}`)
-  //     .then(response => response.json())
-  //     .then(json => setItems(json))
-  // }, [resourceType])
-
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
-
-  const handleWindowWidth = () => {
-    setWindowWidth(window.innerWidth)
+  const [number, setNumber] = useState(0)
+  const [dark, setDark] = useState(false)
+  const doubleNumber = slowFunction(number)
+  const themeStyles = {
+    backgroundColor: dark ? 'black' : 'white',
+    color: dark ? 'white' : 'black'
   }
-
-  useEffect(() => {
-    window.addEventListener('resize', handleWindowWidth)
-
-    return () => { 
-      window.removeEventListener('resize', handleWindowWidth) //this return function will code run first for clean up, for more refer below you tube video
-    }//https://www.youtube.com/watch?v=0ZJgIjIuY7U&list=PLZlA0Gpn_vH8EtggFGERCwMY5u5hOjf-h&index=2
-  }, [])
   return (
-    // <>
-    //   <div className="container">
-    //     <button className="my-2" onClick={() => setResourceType("posts")}>Posts</button>
-    //     <button className="my-2" onClick={() => setResourceType("users")}>Users</button>
-    //     <button className="my-2" onClick={() => setResourceType("comments")}>Comments</button>
-    //     <br />
-    //     {resourceType}
-
-    //     {items.map(item => {
-    //       return <pre>{JSON.stringify(item)}</pre>
-    //     })}
-    //   </div>
-    // </>
-
-    <div className="container">
-      {windowWidth}
-    </div>
+    <>
+      <input type="number" value={number} onChange={e => setNumber(parseInt(e.target.value))} />
+      <button onClick={() => setDark(prevDark => !prevDark)}>Change Theme</button>
+      <div style={themeStyles}>{doubleNumber}</div>
+    </>
   );
+}
+
+function slowFunction(num){
+  console.log("calling slow")
+  for(let i = 0; i <= 1000000000; i++){}
+  return num * 2
 }
