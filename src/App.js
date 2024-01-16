5d26364bb97d86e8009ee1e86ebccf067b5ea4a9
bhumit-bacancy <79578114+bhumit-bacancy@users.noreply.github.com>
2024-01-16 16:44:29 +0530

    useTransition hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import React, { useCallback, useReducer, useState } from "react";
-import useLocalStorage from "./components/useLocalStorage";
-import useUpdateLogger from "./components/useUpdateLogger";
+import React, { useReducer, useState, useTransition } from "react";
 
 function App() {
-  const [name, setName] = useLocalStorage('name', '');
-  useUpdateLogger(name)
+  const [isPending, startTransition] = useTransition()
+  const [input, setInput] = useState("")
+  const [list, setList] = useState([])
+
+  const LIST_SIZE = 20000
+
+  function handleChange(e) {
+    setInput(e.target.value)
+    startTransition(() => {
+      const l = []
+      for (let i = 0; i < LIST_SIZE; i++) {
+        l.push(e.target.value)
+      }
+      setList(l)
+    })
+  }
   return (
     <>
-      <div className="container">
-        <div>
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
-      </div>
+    <div className="container">
+      <input type="text" value={input} onChange={handleChange} />
+      {isPending ? "Loading...." : list.map((item, index) => {
+        return <div key={index}>{item}</div>
+      })}
+    </div>
     </>
-  );
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,15 +4,14 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, HashRouter, MemoryRouter } from "react-router-dom";
-import Modal from "./components/Modal";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <BrowserRouter>
-      <Modal />
+      <App />
     </BrowserRouter>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
