b06d2158f54d913296ee6d1295f1d9c1244d2336
bhumit-bacancy <79578114+bhumit-bacancy@users.noreply.github.com>
2024-01-16 14:50:29 +0530

    usereducer hook with todos app

--- src/App.js
+++ src/App.js
@@ -1,27 +1,55 @@
-import React, { useState } from "react";
-import "./App.css";
-import About from "./components/About";
-import NavBar from "./components/NavBar";
-import TextForm from "./components/TextForm";
-import Home from "./components/Home";
-import { Routes, Route, Outlet } from 'react-router-dom';
-import BookList from "./components/BookList";
-import Book from "./components/Book";
-import NewBook from "./components/NewBook";
-import BookLayout from "./components/BookLayout";
-import BookRoute from "./BookRoute";
-import NotFound from "./components/NotFound";
-import DemoHook from "./components/DemoHook";
-import ClassComponent from "./components/ClassComponent";
-import FunctionComponent from "./components/FunctionComponent";
-import { ThemeProvider } from "./components/ThemeContext";
+import React, { useReducer, useState } from "react";
+import Todo from "./components/Todo";
+
+export const ACTIONS = {
+  ADD_TODO: 'add-todo',
+  TOGGLE_TODO: 'toggle-todo',
+  DELETE_TODO: 'delete-todo'
+}
+
+function reducer(todos, action) {
+  switch (action.type) {
+    case ACTIONS.ADD_TODO:
+      return [...todos, newTodo(action.payload.name)]
+    case ACTIONS.TOGGLE_TODO:
+      return todos.map(todo => {
+        if (todo.id === action.payload.id){
+          return { ...todo, complete: !todo.complete}
+        }
+        return todo
+      })
+    case ACTIONS.DELETE_TODO:
+      return todos.filter(todo => todo.id !== action.payload.id)
+      default:
+        return todos
+  }
+}
+
+function newTodo(name) {
+  return {id: Date.now(), name: name, complete: false}
+}
 
 function App() {
+  const [todos, dispatch] = useReducer(reducer, [])
+  const [name, setName] = useState('')
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    dispatch({ type: ACTIONS.ADD_TODO, payload: {name: name}})
+    setName('')
+  }
+
+  console.log(todos)
   return (
     <>
-      <ThemeProvider>
-        <FunctionComponent />
-      </ThemeProvider>
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={name} onChange={e => setName(e.target.value)} />
+      </form>
+      {todos.map(todo => {
+        return <Todo key={todo.id} todo={todo} dispatch={dispatch}/>
+      })}
+    </div>
     </>
   )
 }
